cc8e6899d5fdd74b774f94f434a1a7643c021a87
whiteheart313 <ammarhamed64@gmail.com>
2025-09-13 12:11:03 +0300

    feat(router): add protected landing route

--- client/auth-fe/src/router/index.tsx
+++ client/auth-fe/src/router/index.tsx
@@ -1,12 +1,23 @@
 import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
 import AuthLayout  from '@/Layout/AuthLayout'
 import GuestRoute from '@/guards/GuestRoute'
+import ProtectedRoute from '@/guards/ProtectedRoute'
 import Login from "@/pages/Login";
 import LoginIllustration from "@/assets/media/webp/illustration-2.webp";
 import SignUpIllustration from "@/assets/media/webp/illustration.webp";
 import SignUp from "@/pages/SignUp";
+import Landing from "@/pages/Landing";
 
 export const router = createBrowserRouter([
+
+	{
+		path:"/", 
+		element: (
+			<ProtectedRoute>
+				<Landing/>
+			</ProtectedRoute>
+		)
+	},
     {
         path: "auth",
 		element: (
